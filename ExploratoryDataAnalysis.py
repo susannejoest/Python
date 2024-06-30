96b84e3a296feb5c3fd6f71691f6bf283a5a4e53
Susanne J <74077559+susannejoest@users.noreply.github.com>
2024-06-30 07:49:39 +0200

    Create ExploratoryDataAnalysis.py

---
+++ ExploratoryDataAnalysis.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+import seaborn as sns
+plt.style.use('ggplot')
+pd.set_option('max_columns', 200)
+df = pd.read_csv('../sample.csv')
